1cc4b54ef7899b91b29958aa5292829b5d86f6c4
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-07-12 00:27:37 +0800

    refactor: adjust player size

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -4,9 +4,22 @@ extends Control
 @onready var weapon_label = $WeaponLabel
 @onready var camera = get_viewport().get_camera_2d()
 
+var hp = 100
+var weapon = "Pistol"
+
+func _ready():
+	update_hud(hp, weapon)
+
 func update_hud(hp, weapon):
 	hp_label.text = "HP: " + str(hp)
 	weapon_label.text = "Weapon: " + weapon
+
+func take_damage(lost_hp):
+	hp -= lost_hp
+	update_hud(hp, weapon)
+
+func set_weapon(new_weapon):
+	update_hud(hp, new_weapon)
 
 func _process(delta):
 	if camera:
