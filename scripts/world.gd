091df80ffd52e875762e858ad775bad6cb8a88aa
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-07-27 12:16:35 +0800

    feat: added map to main world

--- scripts/world.gd
+++ scripts/world.gd
@@ -4,6 +4,6 @@ extends Node2D  # or whatever your main scene extends
 @onready var hud = $HUD
 
 func _ready():
-    weapon.connect("take_damage", Callable(hud, "take_damage"))
-    weapon.connect("set_weapon", Callable(hud, "set_weapon"))
-    weapon.connect("set_ammo", Callable(hud, "set_ammo"))
+	weapon.connect("take_damage", Callable(hud, "take_damage"))
+	weapon.connect("set_weapon", Callable(hud, "set_weapon"))
+	weapon.connect("set_ammo", Callable(hud, "set_ammo"))
