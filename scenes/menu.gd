a57f158960fdd069561439a24e5e1a85b8ddeb5e
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2024-08-09 18:45:14 +0800

    increased barrier size to make zombie pass easier

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -7,7 +7,7 @@ func _ready():
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/map/house_map.tscn")
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 
 func _on_quit_button_pressed():
